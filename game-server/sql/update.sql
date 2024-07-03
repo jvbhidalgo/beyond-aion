4985bdec5f65d4f1e50d232288c5c545981a7545
Farlon <farlon@beyond-aion.com>
2024-07-03 10:36:57 +0200

    Summer events 2024

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -1,5 +1,8 @@
 /*
- * DB changes since a152e86 (09.06.2024)
+* DB changes since a152e86 (09.06.2024)
  */
 
 DROP TABLE `player_vars`;
+
+-- remove old event items
+DELETE FROM inventory WHERE item_id IN (186000111, 188051090, 188051091,188051092, 188051093, 188052625, 188052626, 188052627, 188100124, 188100125);
